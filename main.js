a8011c112a7d872e905e479511f1c769f4921a9f
Alondra Warren <daisy.codeimmersives.com>
2021-04-11 17:15:50 -0700

    format weather app in a card, query weather spec IDs and reconfigure layout

--- main.js
+++ main.js
@@ -1,43 +1,42 @@
+const dogButtonImg = document.getElementById('dog-button-img');
+
+
 const dogButton = document.getElementById('dog');
-dogButton.addEventListener('click', dogButton);
-
-const dogApi = 'https://dog.ceo/dog-api/'; //this holds URL for dog api
-dogApi.addEventListener('click', dogApi);
+dogButton.addEventListener('click', function() {
+    const dogApi = 'https://dog.ceo/api/breeds/image/random';
+    
     fetch(dogApi)
-        .then((data) => data.json())
-        .then(('https://dog.ceo/api/breeds/image/random') => {
+        .then((function (rawResponse) {
+        // console.log('response success!')
+        // console.log('RAW RESPONSE', rawResponse);
+        return rawResponse.json();
+    }))
+        .then((function (json) {
+        dogButtonImg.src = json.message
+        console.log(json);
+    })
+    )})
+    
+    
+    
+    /// Divider /////////
+    
+    const weatherButton = document.getElementById('weather-button');
+    weatherButton.addEventListener('click', function() {
+    const cityInfo = document.getElementById('city-info').value;
+    const weatherApi = `https://goweather.herokuapp.com/weather/${cityInfo}`; //this holds the URL for weather api
+    
+    const temperature = document.getElementById('temperature');
+    
+    const wind = document.getElementById('wind');
 
-        })
-        .catch((error() => console.log(error));
+    const description = document.getElementById('description');
 
-
-    //console.log('Dogs ran!')
-
-//Divider /////////
-
-const weatherButton = document.getElementById('weather');
-weatherButton.addEventListener('click', weatherButton);
-
-const weather = document.getElementById('weather');
-weather.addEventListener('click', weather);
-
-const cityInfo = document.getElementById('city-info').value;
-cityInfo.addEventListener('click', cityInput);
-
-const weatherApi = 'https://goweather.herokuapp.com/weather/{city}'; //this holds the URL for weather api
-weatherApi.addEventListener('click', weatherApi);
-        fetch(weatherApi)
-            .then((data) => data.json())
-            .then(())
-
-const temperature = document.getElementById('temperature');
-temperature.addEventListener('click', temperature);
-
-const wind = document.getElementById('click', wind);
-wind.addEventListener('click', wind);
-
-const description = document.getElementById('description');
-description.addEventListener('click', description);
-
-
-    //console.log('Weather ran!')
+    fetch(weatherApi)
+        .then((function (rawResponse) {
+            return rawResponse.json();
+        }))
+        .then((function (json) {
+            cityInfo = json
+        }))
+    })
